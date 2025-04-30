b5409e3219ff10e9617c7f2386684b7f025071de
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-30 16:18:24 -0300

    feature-002 - feat: Criando migration de Task e Task Option

---
+++ src/main/resources/db/migration/V3__createTableTask.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Task (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    statement VARCHAR(255) NOT NULL,
+    task_order INT NOT NULL,
+    course_id BIGINT NOT NULL,
+    task_type VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
---
+++ src/main/resources/db/migration/V4__createTableTaskOption.sql
@@ -0,0 +1,8 @@
+CREATE TABLE TaskOption(
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    option_name varchar(50) NOT NULL,
+    isCorrect BOOLEAN,
+    task_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_task FOREIGN KEY (task_id) REFERENCES task(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
