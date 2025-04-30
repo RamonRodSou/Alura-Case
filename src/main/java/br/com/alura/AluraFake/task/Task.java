60af676b35e24d5f9bbecfd453b9b87cf20d041a
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-30 16:18:45 -0300

    feature-003 - feat: Criando Task e Task Option

---
+++ src/main/java/br/com/alura/AluraFake/task/Task.java
@@ -0,0 +1,81 @@
+package br.com.alura.AluraFake.task;
+
+import br.com.alura.AluraFake.task.multipleChoise.Multiplechoice;
+import br.com.alura.AluraFake.task.openText.OpenText;
+import br.com.alura.AluraFake.task.singleChoice.SingleChoice;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import jakarta.persistence.*;
+
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = OpenText.class, name = "OPEN_TEXT"),
+        @JsonSubTypes.Type(value = SingleChoice.class, name = "SINGLE_CHOICE"),
+        @JsonSubTypes.Type(value = Multiplechoice.class, name = "MULTIPLE_CHOICE")
+})
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "task_type", discriminatorType = DiscriminatorType.STRING)
+public abstract class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String statement;
+    @Column(name = "task_order")
+    private int order;
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "task_type", insertable = false, updatable = false)
+    private TaskType taskType;
+
+    public Task() { }
+
+    public Task(Long id, String statement, int order, Long courseId, TaskType taskType) {
+        this.id = id;
+        this.statement = statement;
+        this.order = order;
+        this.courseId = courseId;
+        this.taskType = taskType;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int taskOrder) {
+        this.order = taskOrder;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(TaskType taskType) {
+        this.taskType = taskType;
+    }
+}
---
+++ src/main/java/br/com/alura/AluraFake/taskOption/TaskOption.java
@@ -0,0 +1,62 @@
+package br.com.alura.AluraFake.taskOption;
+
+import br.com.alura.AluraFake.task.Task;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class TaskOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "option_name", nullable = false)
+    @Size(min = 4, max = 80)
+    String option;
+
+    boolean isCorrect;
+
+    @ManyToOne
+    private Task task;
+
+    public TaskOption() { }
+
+    public TaskOption(Long id, String option, boolean isCorrect, Task task) {
+        this.id = id;
+        this.option = option;
+        this.isCorrect = isCorrect;
+        this.task = task;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(boolean correct) {
+        this.isCorrect = correct;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}
