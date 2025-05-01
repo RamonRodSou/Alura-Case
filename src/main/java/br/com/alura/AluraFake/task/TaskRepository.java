836bd2816cdbcd0f4dc184a6b9de0364c78b8f06
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-05-01 15:55:36 -0300

    feature-011 - feat: criando query findByCourseIdOrderByOrderAsc em TaskRepository

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -1,13 +1,12 @@
 package br.com.alura.AluraFake.task;
 
-import br.com.alura.AluraFake.taskOption.TaskOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    List<Task> findByCourseId(Long courseId);
+    List<Task> findByCourseIdOrderByOrderAsc(Long courseId);
 
     boolean existsByStatementIgnoreCaseAndCourseId(String statement, Long courseId);
 }
