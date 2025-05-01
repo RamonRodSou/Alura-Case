d75db970bd0de759a4a34f23570302c789300b08
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-05-01 14:23:11 -0300

    feature-010 - refactor: Query do TaskRepository para querys jpa

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -1,13 +1,13 @@
 package br.com.alura.AluraFake.task;
 
+import br.com.alura.AluraFake.taskOption.TaskOption;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    @Query("SELECT t.order FROM Task t WHERE t.courseId = :course")
-    List<Integer> findTaskOrdersByCourse(@Param("course") Long courseId);
+    List<Task> findByCourseId(Long courseId);
+
+    boolean existsByStatementIgnoreCaseAndCourseId(String statement, Long courseId);
 }
