c4e40bf94e004cec6e0d0eff48bfc102e2cc521a
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-05-02 12:47:49 -0300

    feature-016 - feat: Criando o CourseFullExecption a classe do curso que vai receber e passar os status do erro e a mensagem

---
+++ src/main/java/br/com/alura/AluraFake/exepctions/CourseFullException.java
@@ -0,0 +1,21 @@
+package br.com.alura.AluraFake.exepctions;
+
+import org.springframework.http.HttpStatus;
+
+public class CourseFullException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public CourseFullException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public static CourseFullException notFound(String message) {
+        return new CourseFullException(message, HttpStatus.NOT_FOUND);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
