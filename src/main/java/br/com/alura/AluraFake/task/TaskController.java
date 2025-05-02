3dec6d9695f1f4a9f473edd275229e8d92390b34
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-05-02 12:45:22 -0300

    feature-013 - refactor: Colocando notacao transactional em postMapping no TaskController

--- src/main/java/br/com/alura/AluraFake/task/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskController.java
@@ -9,6 +9,7 @@ import br.com.alura.AluraFake.task.singleChoice.SingleChoice;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,18 +24,21 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Transactional
     @PostMapping("/task/new/opentext")
     public ResponseEntity<OpenText> save(@RequestBody @Valid NewOpenTextDTO taskDTO) {
         OpenText openText = taskDTO.toEntity();
         return ResponseEntity.ok((OpenText) taskService.save(openText));
     }
 
+    @Transactional
     @PostMapping("/task/new/singlechoice")
     public ResponseEntity<SingleChoice> save(@RequestBody @Valid NewSingleChoiceDTO taskDTO) {
         SingleChoice singleChoice = taskDTO.toEntity();
         return ResponseEntity.ok((SingleChoice) taskService.save(singleChoice));
     }
 
+    @Transactional
     @PostMapping("/task/new/multiplechoice")
     public ResponseEntity<Multiplechoice> save(@RequestBody @Valid NewMultiplechoiceDTO taskDTO) {
         Multiplechoice  multiplechoice = taskDTO.toEntity();
